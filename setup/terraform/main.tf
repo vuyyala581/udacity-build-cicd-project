3cc68fa645cb523f16d31fadcaa002d8052f3920
vuyyala581 <vuyyala581@users.noreply.github.com>
2026-09-05 12:52:18 +0000

    Add frontend CD workflow and update EKS configuration

--- setup/terraform/main.tf
+++ setup/terraform/main.tf
@@ -187,9 +187,7 @@ resource "aws_iam_role_policy_attachment" "eks_service" {
 # EKS Node Group
 ##################
 # Track latest release for the given k8s version
-data "aws_ssm_parameter" "eks_ami_release_version" {
-  name = "/aws/service/eks/optimized-ami/${aws_eks_cluster.main.version}/amazon-linux-2/recommended/release_version"
-}
+
 
 resource "aws_eks_node_group" "main" {
   node_group_name = "udacity"
@@ -197,8 +195,8 @@ resource "aws_eks_node_group" "main" {
   version         = aws_eks_cluster.main.version
   node_role_arn   = aws_iam_role.node_group.arn
   subnet_ids      = [var.enable_private == true ? aws_subnet.private_subnet.id : aws_subnet.public_subnet.id]
-  release_version = nonsensitive(data.aws_ssm_parameter.eks_ami_release_version.value)
-  instance_types  = ["t3.small"]
+
+  instance_types = ["t3.small"]
 
   scaling_config {
     desired_size = 1
@@ -316,10 +314,6 @@ resource "aws_iam_user" "github_action_user" {
   name = "github-action-user"
 }
 
-resource "aws_iam_user_policy" "github_action_user_permission" {
-  user   = aws_iam_user.github_action_user.name
-  policy = data.aws_iam_policy_document.github_policy.json
-}
 
 data "aws_iam_policy_document" "github_policy" {
   statement {
@@ -328,3 +322,6 @@ data "aws_iam_policy_document" "github_policy" {
     resources = ["*"]
   }
 }
+
+
+
